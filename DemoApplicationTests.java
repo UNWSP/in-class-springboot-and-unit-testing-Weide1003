1fb948ab4d641685e6f1af5b9018ac51093953be
Weide1003 <112137004+Weide1003@users.noreply.github.com>
2022-12-01 20:54:24 -0600

    Add files via upload

---
+++ DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+@SpringBootTest
+class DemoApplicationTests {
+@Test
+void contextLoads( ) {
+@Test
+void testoetContent({
+Greeting grceting = new Greeting(i,"Test");
+assertEquals( "Test" , greeting.getcontent());
+}
+}
+
